4ddf742dba98ec38b15ba134464d37d0afd849ce
aspiringguru <bmatthewtaylor@gmail.com>
2019-08-08 10:50:25 +1000

    end, works, deviation from youtube examples.

--- news-article.js
+++ news-article.js
@@ -1,6 +1,27 @@
 class NewsArticle extends HTMLElement {
+    constructor() {
+        super();
+        this.root = this.attachShadow({ mode: 'open' });
+    }
+
+    //need css styles inside this below as styles.css not accessible otherwise.
     set article(article) {
-    this.innerHTML = `
+    this.root.innerHTML = `
+      <style>
+         h2 {
+          font-family: Georgia, 'Times New Roman', Times, serif;
+        }
+
+         a,
+         a:visited {
+          text-decoration: none;
+          color: inherit;
+        }
+
+         img {
+          width: 100%;
+        }
+      </style>
       <a href="${article.url}">
         <h2>${article.title}</h2>
         <img src="${article.urlToImage ? article.urlToImage : ''}">
